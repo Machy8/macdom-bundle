830f885ef4a83dcd7357a3e67ca4bf7dfc3d7e6f
Vladimír Macháček <8machy@seznam.cz>
2018-02-01 21:15:49 +0100

    - Added MacdomExtension.php

---
+++ DependencyInjection/MacdomExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *
+ * Copyright (c) Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Macdom\MacdomBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+
+class MacdomExtension extends Extension
+{
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function load(array $configs, ContainerBuilder $container)
+	{
+		$loader = new YamlFileLoader(
+			$container,
+			new FileLocator(__DIR__.'/../Resources/config')
+		);
+
+		$loader->load('services.yml');
+	}
+
+}
