d67b512891215c544dfaee116ff9ccca6c83a133
Vladimír Macháček <8machy@seznam.cz>
2018-02-01 21:15:26 +0100

    - Added MacdomBundle.php

---
+++ MacdomBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *
+ * Copyright (c) Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Macdom\MacdomBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class MacdomBundle extends Bundle
+{
+
+}
